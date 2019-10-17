7df7b3eb925d535839cf550cbd5a9189ba32b3d5
Renjie Sun <sunrenjie6@gmail.com>
2019-10-17 22:12:29 +0800

    Initial import of the FraudDetection project
    The switch from gradle to maven is to make our life easier in many ways.

---
+++ WebConsole/src/main/java/io/pivotal/demo/sko/ui/WebConsoleApp.java
@@ -0,0 +1,16 @@
+package io.pivotal.demo.sko.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class WebConsoleApp {
+
+	// Zip codes US MAP: https://gist.github.com/jefffriesen/6892860
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebConsoleApp.class, args);
+    }
+}
